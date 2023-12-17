512c2deedb88206296735a70b6d72c61c7849d25
Rajrishi07 <130096898+Rajrishi07@users.noreply.github.com>
2023-12-17 23:55:59 +0530

    Add files via upload

--- youtube-summariser/app.py
+++ youtube-summariser/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+from transformers import BartForConditionalGeneration, BartTokenizer
 from youtube_transcript_api import YouTubeTranscriptApi
-from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-# Load the T5 model and tokenizer
-model_name = "t5-base"
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+# Load the pre-trained BART model and tokenizer for summarization
+model_name = "facebook/bart-large-cnn"
+model = BartForConditionalGeneration.from_pretrained(model_name)
+tokenizer = BartTokenizer.from_pretrained(model_name)
 
 app = Flask(__name__)
 
@@ -20,7 +21,8 @@ def transcribe():
     if request.method == 'POST':
         transcribe_text = ""
         link = request.form['video_id']
-        response = YouTubeTranscriptApi.get_transcript(link)
+        print(link)
+        response = YouTubeTranscriptApi.get_transcript(extract_video_id(link))
         for n in range(0, len(response)):
             transcribe_text = transcribe_text + response[n]['text'] + " "
         print(transcribe_text)
@@ -31,8 +33,12 @@ def transcribe():
         return render_template('index.html')
 
 
-def summarise(transcript, max_chunk_size=512):
+def extract_video_id(youtube_url):
+    # Extract video ID from YouTube URL
+    video_id = youtube_url.split("be/")[1]
+    return video_id
 
+def summarise(transcript, max_chunk_size=1024):
     # Split the transcript into chunks
     transcript_chunks = [transcript[i:i + max_chunk_size] for i in range(0, len(transcript), max_chunk_size)]
 
@@ -41,17 +47,15 @@ def summarise(transcript, max_chunk_size=512):
 
     # Summarize each chunk
     for chunk in transcript_chunks:
-        # Add the T5 specific prefix "summarize: "
-        input_text = "summarize: " + chunk
+        print(f"{chunk}\n\n\n")
+        input_ids = tokenizer.encode(chunk, return_tensors="pt", max_length=1024, truncation=True)
 
-        # Tokenize and generate the summary
-        inputs = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
-        summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4,
-                                     early_stopping=True)
+        # Generate summary
+        summary_ids = model.generate(input_ids, max_length=150, length_penalty=2.0, num_beams=4, early_stopping=True)
 
-        # Decode the generated summary and append to the list of summaries
-        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-        summaries.append(summary)
+        # Decode and append the generated summary
+        generated_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        summaries.append("->--"+generated_summary+"\n")
 
     # Concatenate the individual summaries to get the overall summary
     overall_summary = " ".join(summaries)
